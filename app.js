f63b9275c548525c0ece803114c571b6f1beb317
yodji09 <yodji09@gmail.com>
2020-05-03 00:55:34 +0700

    Training

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
+if(process.env.NODE_ENV == 'development'){
+    require('dotenv').config()
+}
+
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 const cors = require('cors')
 const router = require('./routes/index')
 const errHandler = require('./middlewares/errHandler')
-require('dotenv').config()
 
 app.use(express.urlencoded({extended : true}))
 app.use(router)
